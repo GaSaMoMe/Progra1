7183e5099acb6be8cdc78a294232f9097eceb21c
GaSaMoMe <78625190+GaSaMoMe@users.noreply.github.com>
2021-04-16 00:21:12 -0600

    Add files via upload
    Problema 1 resuelto en Tester

---
+++ TesterProyecto.java
@@ -0,0 +1,23 @@
+/*Universidad Latina de Costa Rica
+ * Programacion#1
+ *Profesor Carlos Andres Mendez Rodriguez
+ * Facultad de Ingenieria en Sistemas Computacionales
+ *Suponiendo que el cambio bancario por 1 dólar equivale a 500 colones y que por un euro equivale a 1.53 dólares diseñe e implemente un programa orientado a objetos que le permita al usuario seleccionar la cantidad de unidades monetarias y el tipo de moneda de origen, así como el tipo de moneda de salida que es deseada.
+ */
+package ProyectoFinal;
+
+/**
+ *
+ * @author GasaMome
+ */
+public class TesterProyecto {
+    
+    public static void main(String[]args){
+        //Tester problema 1
+
+problema1 g= new problema1();
+    
+    g.metodoFinal();
+    }
+   
+}
